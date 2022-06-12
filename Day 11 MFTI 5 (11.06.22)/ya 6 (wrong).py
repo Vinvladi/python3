095a8e4f37bc9b1199f7e83edb703e995902cd5a
vinvladi <vladislavsupra@yandex.ru>
2022-06-12 09:44:34 +0300

    Update ya 6 (wrong).py

--- Day 11 MFTI 5 (11.06.22)/ya 6 (wrong).py
+++ Day 11 MFTI 5 (11.06.22)/ya 6 (wrong).py
@@ -1,6 +1,9 @@
-j,n = map(str,input().split())
-i = len(j)
-d = 0
-if j[0] == n:
-    d += 1
-print (d)   
+# j,n = map(str,input().split()) # не работает тест xxo ukzxlzxlnqoplmvwltlkvatmnnlllxrzcuqlhgomacnpeudfjpvliodepczv # удалить одинаковые элементы
+j = 'xxo'
+n = 'ukzxlzxlnqoplmvwltlkvatmnnlllxrzcuqlhgomacnpeudfjpvliodepczv'
+k = 0                                       
+for i1 in range(0,len(j),1):
+    for i2 in range (0,len(n),1):
+        if (j[i1] in n[i2]) == True:
+            k +=1
+print (k)
