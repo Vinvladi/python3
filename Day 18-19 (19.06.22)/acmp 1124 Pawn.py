5934b5d04e8dcd17fa418630c88d1ce19c16dbf7
vinvladi <vladislavsupra@yandex.ru>
2022-06-20 00:07:39 +0300

    Delete acmp 1124 Pawn.py

--- Day 18-19 (19.06.22)/acmp 1124 Pawn.py
+++
@@ -1,9 +0,0 @@
-a,b = map(int,input().split())
-c,d = map(int,input().split())
-if 1<=a<=8 and 1<=c<=8 and 1<=c<=8 and 1<=d<=8:
-    if a == c or b == d:
-        print("YES")
-    else:
-        print("NO")
-else:
-    print("NO")
