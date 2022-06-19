8a1ce6a10af8299751767fd0f08313ae76d43db1
vinvladi <vladislavsupra@yandex.ru>
2022-06-20 00:07:32 +0300

    Update acmp 1121 Queen.py

--- Day 18-19 (19.06.22)/acmp 1121 Queen.py
+++ Day 18-19 (19.06.22)/acmp 1121 Queen.py
@@ -1,7 +1,11 @@
+from base64 import b16encode
+
+
 a,b = map(int,input().split())
 c,d = map(int,input().split())
-if 1<=a<=8 and 1<=c<=8 and 1<=c<=8 and 1<=d<=8:
-    if a == c or b == d:
+if 1<=a<=8 and 1<=b<=8 and 1<=c<=8 and 1<=d<=8:
+# 4,5 (7,2)    
+    if a+b==c+d or a-b == c-d or a == c or b == d:
         print("YES")
     else:
         print("NO")
