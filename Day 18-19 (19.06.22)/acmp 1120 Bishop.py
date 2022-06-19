1edc773f9a520ed8784b68aad228889560962621
vinvladi <vladislavsupra@yandex.ru>
2022-06-19 23:19:53 +0300

    Create acmp 1120 Bishop.py

---
+++ Day 18-19 (19.06.22)/acmp 1120 Bishop.py
@@ -0,0 +1,10 @@
+a,b = map(int,input().split())
+c,d = map(int,input().split())
+if 1<=a<=8 and 1<=b<=8 and 1<=c<=8 and 1<=d<=8:
+# 4,5 (7,2)    
+    if a+b==c+d or a-b == c-d:
+        print("YES")
+    else:
+        print("NO")
+else:
+    print("NO")
