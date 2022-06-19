78dddc703a9ebe081b6aaa9f5b3149a7e9721d5a
vinvladi <vladislavsupra@yandex.ru>
2022-06-20 00:07:34 +0300

    Update acmp 1122 King.py

--- Day 18-19 (19.06.22)/acmp 1122 King.py
+++ Day 18-19 (19.06.22)/acmp 1122 King.py
@@ -1,7 +1,8 @@
 a,b = map(int,input().split())
 c,d = map(int,input().split())
-if 1<=a<=8 and 1<=c<=8 and 1<=c<=8 and 1<=d<=8:
-    if a == c or b == d:
+if 1<=a<=8 and 1<=b<=8 and 1<=c<=8 and 1<=d<=8:
+# 4,5 (7,2)    
+    if a-1<=c<=a+1 and b-1<=d<=b+1:
         print("YES")
     else:
         print("NO")
