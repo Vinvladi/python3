6dc3add96e42d5ff48ac8913f34eaea9c539d7e3
vinvladi <vladislavsupra@yandex.ru>
2022-06-20 00:36:14 +0300

    Create ego 16 if if task 4.py

---
+++ Day 18-19 (19.06.22)/ego 16 if if task 4.py
@@ -0,0 +1,11 @@
+a = int(input())
+if a%4==0:
+    print('ost 0')
+else:
+    if a%4 ==0:
+        print("ost 1")
+    else:
+        if a%4==2:
+            print('ost 2')
+        else:
+            print('ost 3')
