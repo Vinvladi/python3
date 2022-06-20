52f13ca6b10cc4460d0ed49bd1c459fd8865dfcb
vinvladi <vladislavsupra@yandex.ru>
2022-06-20 10:29:32 +0300

    Update acmp 1119 Rook .py

--- Day 18-19 (19.06.22)/acmp 1119 Rook .py
+++ Day 18-19 (19.06.22)/acmp 1119 Rook .py
@@ -1,6 +1,9 @@
-a,b = map(int,input().split)
-c,d = map(int,input().split)
-if a == c or b == d:
-    print("YES")
+a,b = map(int,input().split())
+c,d = map(int,input().split())
+if 1<=a<=8 and 1<=b<=8 and 1<=c<=8 and 1<=d<=8:
+    if a == c or b == d:
+        print("YES")
+    else:
+        print("NO")
 else:
     print("NO")
