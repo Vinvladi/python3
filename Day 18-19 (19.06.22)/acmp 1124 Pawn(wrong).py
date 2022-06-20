167c65db1eb75dec0a969b765eadf620f5f15cdb
vinvladi <vladislavsupra@yandex.ru>
2022-06-20 10:29:34 +0300

    Update acmp 1124 Pawn(wrong).py

--- Day 18-19 (19.06.22)/acmp 1124 Pawn(wrong).py
+++ Day 18-19 (19.06.22)/acmp 1124 Pawn(wrong).py
@@ -1,9 +1,15 @@
 a,b = map(int,input().split())
 c,d = map(int,input().split())
-if 1<=a<=8 and 1<=c<=8 and 1<=c<=8 and 1<=d<=8:
-    if a == c or b == d:
-        print("YES")
+if 1<=a<=8 and 1<=b<=8 and 1<=c<=8 and 1<=d<=8:
+    if b == 2:
+        if a == c and (b+2 == d or b+1 ==d):
+            print("YES")
+        else:
+            print("NO")
     else:
-        print("NO")
+        if a == c and (b+1 == d) :
+            print("YES")
+        else:
+            print("NO")
 else:
     print("NO")
