5c159b0b76d190ce2f49701017c11acd997c2191
vinvladi <vladislavsupra@yandex.ru>
2022-06-20 10:29:35 +0300

    Update ego 15 number comparison a,b.py

---
+++ Day 18-19 (19.06.22)/ego 15 number comparison a,b.py
@@ -0,0 +1,11 @@
+a,b = map(int,input().split())
+if a>b:
+    print(a)
+else:
+    print(b)
+# далее деление на четное/нечетное
+if a%2==0:
+    print("Четное")
+else:
+    print("Нечетное")
+# обе программы прогоняет 
