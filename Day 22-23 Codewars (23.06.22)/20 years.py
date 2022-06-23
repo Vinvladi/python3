e461fc62241ffd302b5c865ab78680ed412ba391
vinvladi <vladislavsupra@yandex.ru>
2022-06-23 23:52:38 +0300

    Create 20 years.py

---
+++ Day 22-23 Codewars (23.06.22)/20 years.py
@@ -0,0 +1,25 @@
+def stairs_in_20(stairs):
+    for sunday in stairs[0]:
+        sunday += 1
+    print(stairs)
+"""    for monday in stairs[1]:
+        monday += 1
+    for tuesday in stairs[2]:
+        tuesday += 1
+    for wednesday in stairs[3]:
+        wednesday += 1
+    for thursday in stairs[4]:
+        thursday += 1
+    for friday in stairs[5]:
+        friday += 1
+    for saturday in stairs[6]:
+        saturday += 1
+    return sum(stairs[:])*20
+"""
+
+
+
+sunday = [6737, 7244, 5776, 9826, 7057, 9247, 5842, 5484, 6543, 5153, 6832, 8274,
+          7148, 6152, 5940, 8040, 9174, 7555, 7682, 5252, 8793, 8837, 7320, 8478, 6063,
+          5751, 9716, 5085, 7315, 7859, 6628, 5425, 6331, 7097, 6249, 8381, 5936, 8496,
+          6934, 8347, 7036, 6421, 6510, 5821, 8602, 5312, 7836, 8032, 9871, 5990, 6309, 7825]
