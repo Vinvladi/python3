007b16a688c5b944c3ef9df2e79722fa2abbf7c0
vinvladi <vladislavsupra@yandex.ru>
2022-06-25 04:44:45 +0300

    Create ord.py

---
+++ Day 24 Codewars (24.06.22)/ord.py
@@ -0,0 +1,14 @@
+print(ord('a'))
+print(ord('z'))
+
+print(ord("A"))
+print(ord("Z"))
+string = ("aoker")
+
+if ord(string[0]) < 65 or ord(string[0]) > 90:
+    print("не равно")
+    print(string[0])
+else:
+    print("равно")
+
+print(ord(string[0]))
