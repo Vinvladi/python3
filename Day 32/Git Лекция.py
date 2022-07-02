6af2465de7a85a51ddf0d235ced49a2a4e04a8a3
vinvladi <vladislavsupra@yandex.ru>
2022-07-02 09:57:21 +0300

    Create Git Лекция.py

---
+++ Day 32/Git Лекция.py
@@ -0,0 +1,14 @@
+# Git распределенная система использования версий
+# GitHub - это сервис хостинга Git репозиториев (Основная версия проекта в github)
+#
+# Команды для powershell
+# git --version (показывает версию программы)
+# git version 2.33.0.windows.2 (2 - major, 33 - minor, 0 - path)
+#
+# git update-git-for-windows - обновление версии git
+#
+# После установки git необходимо настроить имя и email автора
+# git config --global user.name <..> - имя
+# git config --global user.email <..>  - почта
+#
+# Остальное в тетрадке 23+ стр
