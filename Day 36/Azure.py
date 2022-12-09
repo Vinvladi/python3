273309f571f11e12c30aa2d37a98dc4b495e6b52
vinvladi <vladislavsupra@yandex.ru>
2022-12-09 10:50:10 +0300

    Create Azure.py

---
+++ Day 36/Azure.py
@@ -0,0 +1,28 @@
+#
+import math
+
+with open (input.txt) as file:
+lines = [line.rstrip() for line in file]
+
+LaunchCooldown = 0
+k = 0
+
+for i in range(1,d,1):
+    ksum =
+    k = ksum + k
+
+LaunchCooldown = ksum*2.2 math.sqrt(200/Reload*(1+StatBonus)+100) +0.1
+
+print (LaunchCooldown)
+
+(a*b+a1*b1+a2*b2) / (b+b1+bz2)
+3
+10.81 2
+11.91 3
+11.17 3
+
+
+10.81s tigercat
+11.91s curtiss
+11.17s
+(10,81 * 1) + (11,91*2) + (11.17*3) / (1+2+3)
