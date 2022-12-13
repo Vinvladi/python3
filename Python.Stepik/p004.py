1c44ede61fdaa283dcd4d8f53184028f71b456a9
vinvladi <vladislavsupra@yandex.ru>
2022-12-13 16:45:15 +0300

    Разделение через % и // числа и запись его через 2 метода (один через кучу print / другой через sep = "\n")

---
+++ Python.Stepik/p004.py
@@ -0,0 +1,16 @@
+i = int(input())
+a1 = i%10000//1000
+print(a1)
+a2 = i%1000//100
+print(a2)
+a3 = i%100//10
+print(a3)
+a4 = i%10
+print(a4)
+
+y = int(input())
+y1 = y%10000//1000
+y2 = y%1000//100
+y3 = y%100//10
+y4 = y%10
+print(y1, y2, y3, y4, sep ="\n")
