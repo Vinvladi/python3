d7eec72e589e3729c927ea5707dbac7349d85ad0
vinvladi <vladislavsupra@yandex.ru>
2022-12-15 19:20:15 +0300

    Глава 1.3 учебника

---
+++ Python 2022 Book (Python Distilled - David M.Breazley)/1.3. Вычисление сложных процентов с f-строкой.py
@@ -0,0 +1,8 @@
+principal = 1000 # Исходная сумма
+rate = 0.05      # Процентная ставка
+numyears = 5     # Количество лет
+years = 1
+while years <= numyears:
+    principal = principal*(1+rate)
+    print(f'{years:>3d} {principal:0.2f}')
+    years +=1
