3099145293ca6e1411650ed27294a8fb381d19b3
vinvladi <vladislavsupra@yandex.ru>
2023-01-04 15:22:10 +0300

    Решение задача на простые арифметические операции в модуле stepik 1.3.1 to 1.3.14

---
+++ Egorov Artem Python 3 @egoroffchannel/ego.s 1.3.x.py
@@ -0,0 +1,42 @@
+a1 = 19 + 3.0
+print(a1) # ответ 1-ая задача
+print(type(a1))
+a2 = 36 / 6
+print(a2) # ответ 2-ая задача
+print(type(a2))
+a3 = 4 + 2 * 5
+print(a3) # ответ 3-ей задачи
+print(type(a3))
+a4 = 56%10
+print(a4) # ответ 4-ой задачи
+print(type(a4))
+a5 = 8%41
+print(a5) # ответ 5-ой задачи
+print(type(a5))
+a6 = 89//15
+print(a6) # ответ 6-ой задачи
+print(type(a6))
+a7 = 17 - 45//8
+print(a7) # ответ 7-ой задачи
+print(type(a7))
+a8 = 2**5
+print(a8) # ответ 8-ой задачи
+print(type(a8))
+a9 = 100/2**2
+print(a9) # ответ 9-ой задачи
+print(type(a9))
+a10 = abs(-45)  # модуль числа (abs)
+print(a10)
+print(type(a10))
+a11 = 16**0.5
+print(a11) # ответ 11-ой задачи
+print(type(a11))
+a12 = abs(-100) + abs(-13)
+print(a12) # ответ 12-ой задачи
+print(type(a12))
+a13 = max(345, 13, 89, 4392, 76) - 392 # max - максимальный элемент в последовательности
+print(a13) # ответ 13-ой задачи
+print(type(a13))
+a14 = round(1/3, 2) # round - оставляет 2 знака после запятой
+print(a14) # ответ 14-ой задачи
+print(type(a14))
