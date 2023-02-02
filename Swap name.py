ae034f5730455fb546f4ec697f8eb58f109129d8
vinvladi <vladislavsupra@yandex.ru>
2023-02-03 00:15:10 +0300

    Дополнение программы swap name

--- Swap name.py
+++ Swap name.py
@@ -1,7 +1,9 @@
+# example : ego.s 2.1.1.py (пробел между именем и этапом курса)
 name = "ego.s"
-stage = "2.8"
-start = 3
-end = 4
+symbol = " "  # символ между name и stage
+stage = "2.9"
+start = 2
+end = 9
 for i in range(start, end+1):
-    with open(f'{name} {stage}.{i}.py', 'w'):
+    with open(f'{name}{symbol}{stage}.{i}.py', 'w'):
         pass
