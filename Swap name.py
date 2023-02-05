a95374ab6ce5879fdea01555f48355e47925b56a
vinvladi <vladislavsupra@yandex.ru>
2023-02-05 14:02:43 +0300

    Просто изменял swap name.py

--- Swap name.py
+++ Swap name.py
@@ -1,9 +1,9 @@
 # example : ego.s 2.1.1.py (пробел между именем и этапом курса)
-name = "ego.s"
+name = "GenPython"
 symbol = " "  # символ между name и stage
-stage = "2.13"
-start = 4
-end = 15
+stage = "4.1"
+start = 6
+end = 12
 for i in range(start, end+1):
     with open(f'{name}{symbol}{stage}.{i}.py', 'w'):
         pass
