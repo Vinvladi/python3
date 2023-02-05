2dad2373bbc0e104853f557ffdcfa1e9f1a9d851
vinvladi <vladislavsupra@yandex.ru>
2023-02-05 07:16:48 +0300

    Готовый экзамен №1 (3 этап курса Поколение Python(курс для начинающих))

---
+++ Education Сourses/Stepik 58852 Поколение Python (курс для начинающих)/3. Экзамен 1/GenPython 3.2.1.b.py
@@ -0,0 +1,8 @@
+h=4
+i=17
+vv = "*"*i
+v1 = "*"*(i-16) + " "*(i-2) + "*"*(i-16)
+print(vv)
+for item in range(2,h+1,1):
+    print(v1)
+print(vv)
---
+++ Education Сourses/Stepik 58852 Поколение Python (курс для начинающих)/3. Экзамен 1/GenPython 3.2.4.py
@@ -0,0 +1,6 @@
+n = int(input())
+n1 = n
+n2 = n*10+n
+n3 = n*100+n*10+n
+n4 = n*100+n*20+3*n
+print(n4)
