1238cee567b7d4c614681f71eefe5edb2b89fb70
vinvladi <vladislavsupra@yandex.ru>
2023-02-26 09:20:05 +0300

    Update

---
+++ Task (The solution of the problem)/Codeforce/Codeforce Round 853 (Div.2)/001.py
@@ -0,0 +1,10 @@
+i = int(input())
+count = 0
+while count < i:
+    len_array = int(input())
+    i_array = list(map(int, input().split()))
+    while a != b:
+        if a > b:
+            a = a - b
+        else:
+            b = b - a
