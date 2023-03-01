8cb20017db0e193152ff730d0956db82ca344395
vinvladi <vladislavsupra@yandex.ru>
2023-03-01 16:24:13 +0300

    Reworking v.1.01

--- Создатель имен по форме (Build name new files).py
+++ Создатель имен по форме (Build name new files).py
@@ -1,5 +1,5 @@
-# example : ego.s 2.1.Leetcode 1. Two Sum .py (пробел между именем и этапом курса)
-name_list = ["ego.s", "mail.deluxe"]
+# example : ego.s 2.1.Leetcode 1. Two Sum .py (пробел между именем и этапом курса), EM.Python 02.01
+name_list = ["ego.s", "mail.deluxe", "EM.Python"]
 name = name_list[0]
 symbol = " "  # символ между name и stage
 stage = "5.9"
