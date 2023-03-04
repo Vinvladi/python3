4c261a15dafb54e69e434cfede8c86b630342b93
vinvladi <vladislavsupra@yandex.ru>
2023-03-04 18:19:52 +0300

    Update book Eric Matthes chapter 2 page 49

---
+++ Book and Lecture Good/Eric Matthes Python Crash Course  2nd Edition/Code.in chapters/EM.Python 02.03.01b split.py
@@ -0,0 +1,6 @@
+s = "wsdadas asdasd as asd asda asd asd"  # Пример 2
+print(s.split(" "))   #данный пример срезает все элементы по вхождению (когда а - элемент встречается) и делает list
+
+""" console:
+['wsd', 'd', 's ', 'sd', 'sd ', 's ', 'sd ', 'sd', ' ', 'sd ', 'sd']
+"""
